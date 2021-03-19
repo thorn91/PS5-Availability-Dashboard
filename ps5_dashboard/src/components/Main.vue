c58ffee862594529e7ef9d401753a82b6202cadf
Thomas Horn <hortw1991@gmail.com>
2021-03-18 23:05:12 -0400

    added title

--- ps5_dashboard/src/components/Main.vue
+++ ps5_dashboard/src/components/Main.vue
@@ -1,11 +1,14 @@
 <template>
- <div id="main-container" class="d-flex justify-content-center">
-    <div class="card" style="width: 24rem;">
-      <ul class="list-group p-1 list-group-flush">
-        <!-- Individual template items have to be separate, too many differences -->
-        <Walmart />
-        <Target />
-      </ul>
+  <div>
+    <h1 id="title" class="text-lg-center">PS5 Availability Dashboard</h1>
+    <div id="main-container" class="d-flex justify-content-center">
+      <div class="card" style="width: 24rem;">
+        <ul class="list-group p-1 list-group-flush">
+          <!-- Individual template items have to be separate, too many differences -->
+          <Walmart />
+          <Target />
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -24,9 +27,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#title {
+  margin-top: 5%;
+}
+
 #main-container {
   height: 100%;
-  margin-top: 25%;
+  margin-top: 20%;
 }
 
 </style>
